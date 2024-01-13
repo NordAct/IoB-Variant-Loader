b562d101c7605cebf32a736a73d093ec320b0820
NordAct <73118477+NordAct@users.noreply.github.com>
2024-01-13 13:33:43 +0300

    Add tooltip

--- src/main/java/nordmods/iobvariantloader/mixin/common/DragonEggItemMixin.java
+++ src/main/java/nordmods/iobvariantloader/mixin/common/DragonEggItemMixin.java
@@ -1,12 +1,18 @@
 package nordmods.iobvariantloader.mixin.common;
 
 import com.GACMD.isleofberk.items.DragonEggItem;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.ServerLevelAccessor;
 import nordmods.iobvariantloader.util.VariantNameHelper;
 import nordmods.iobvariantloader.util.dragon_variant.DragonVariant;
@@ -16,6 +22,7 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
@@ -45,7 +52,7 @@ public abstract class DragonEggItemMixin extends Item {
     @Inject(method = "useOn(Lnet/minecraft/world/item/context/UseOnContext;)Lnet/minecraft/world/InteractionResult;", at = @At("HEAD"))
     private void getVariant(UseOnContext pContext, CallbackInfoReturnable<InteractionResult> cir) {
         ItemStack itemStack = pContext.getItemInHand();
-        if (itemStack.hasTag() && itemStack.getTag().contains("VariantName")) variant = itemStack.getTag().getString("VariantName");
+        if (itemStack.hasTag()) variant = itemStack.getTag().getString("VariantName");
         else variant = "";
     }
 
@@ -58,5 +65,15 @@ public abstract class DragonEggItemMixin extends Item {
             case "m_nightmare" -> "monstrous_nightmare";
             case "nadder" -> "deadly_nadder";
         };
+    }
+
+    @Inject(method = "appendHoverText(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/level/Level;Ljava/util/List;Lnet/minecraft/world/item/TooltipFlag;)V", at = @At("TAIL"))
+    private void addVariantTooltip(ItemStack pStack, Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced, CallbackInfo ci) {
+        String variant = pStack.hasTag() ? pStack.getTag().getString("VariantName") : "";
+        if (!variant.isEmpty()) {
+            String firstLetter = String.valueOf(variant.charAt(0));
+            variant = variant.replaceFirst(firstLetter, firstLetter.toUpperCase());
+            pTooltipComponents.add(new TranslatableComponent("tooltip.iobvariantloader.variant", new TextComponent(variant).withStyle(ChatFormatting.GOLD)));
+        }
     }
 }
