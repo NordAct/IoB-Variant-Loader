8947cc225880b706013fc8aaffc6259c3608f653
NordAct <73118477+NordAct@users.noreply.github.com>
2024-05-30 23:32:49 +0300

    Use inject instead of override to add glow layer rendering

--- src/main/java/nordmods/iobvariantloader/mixin/client/isleofberk/egg/SmallEggRendererMixin.java
+++ src/main/java/nordmods/iobvariantloader/mixin/client/isleofberk/egg/SmallEggRendererMixin.java
@@ -12,6 +12,9 @@ import nordmods.iobvariantloader.util.VLGlowLayerHelper;
 import nordmods.iobvariantloader.util.layer.VLGlowLayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import software.bernie.geckolib3.geo.render.built.GeoModel;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
 import software.bernie.geckolib3.renderers.geo.GeoProjectilesRenderer;
@@ -27,12 +30,9 @@ public abstract class SmallEggRendererMixin extends GeoProjectilesRenderer<ADrag
         super(renderManager, modelProvider);
     }
 
-    @Override
-    public void render(GeoModel model, ADragonSmallEggBase animatable, float partialTicks, RenderType type, PoseStack matrixStackIn,
-                        @Nullable MultiBufferSource renderTypeBuffer, @Nullable VertexConsumer vertexBuilder, int packedLightIn,
-                        int packedOverlayIn, float red, float green, float blue, float alpha) {
-        super.render(model, animatable, partialTicks, type, matrixStackIn, renderTypeBuffer, vertexBuilder, packedLightIn, packedOverlayIn, red, green, blue, alpha);
-        glowLayer.render(matrixStackIn, renderTypeBuffer, packedLightIn, animatable, 0, 0, partialTicks, animatable.getAge(), 0, 0);
+    @Inject(method = "render(Lcom/GACMD/isleofberk/entity/eggs/entity/base/small/ADragonSmallEggBase;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V", at = @At("TAIL"))
+    private void renderGlowLayer(ADragonSmallEggBase entity, float yaw, float partialTicks, PoseStack poseStack, MultiBufferSource buffer, int packedLight, CallbackInfo ci) {
+        glowLayer.render(poseStack, buffer, packedLight, entity, 0, 0, partialTicks, animatable.getAge(), 0, 0);
     }
 
     @Override
@@ -45,6 +45,6 @@ public abstract class SmallEggRendererMixin extends GeoProjectilesRenderer<ADrag
     //*screams*
     @Override
     public ResourceLocation getTextureLocation(ADragonSmallEggBase instance) {
-        return modelProvider.getTextureLocation(instance);
+        return super.getTextureLocation(instance);
     }
 }
